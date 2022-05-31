3c1e40a60a65b96366d54f95e7e954ead9398d8b
alvaro2302 <alvaro230297@hotmail.com>
2022-05-31 18:43:37 -0400

    delete console.log

--- src/services/SearchService.js
+++ src/services/SearchService.js
@@ -4,7 +4,6 @@ async function search(email)
 {
     let url = `/users?email=${email}`;
     const searchResults = await ApiService.get(url);
-    console.log("service search", searchResults)
     return searchResults;
 }
 export default { search}
