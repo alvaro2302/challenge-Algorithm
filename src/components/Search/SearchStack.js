9d6478f938c531919c71de01669d47433c308d2d
alvaro2302 <alvaro230297@hotmail.com>
2022-06-01 12:40:16 -0400

    add componente details for navigate

--- src/components/Search/SearchStack.js
+++ src/components/Search/SearchStack.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createStackNavigator,TransitionPresets } from "@react-navigation/stack";
 import SearchScreen from "./SearchScreen";
 import PostScreen from "../Posts/PostScreen";
+import PostDetails from "../Posts/PostDetails";
 const screenOptions = {
     headerTransparent: true,
     headerTitleStyle: {
@@ -17,7 +18,7 @@ const SearchStack = ()=>{
         <Stack.Navigator initialRouteName='search' screenOptions={screenOptions}>
             <Stack.Screen name='search' component={SearchScreen}/>
             <Stack.Screen name='posts' component={PostScreen}/>
-
+            <Stack.Screen name='detailsPost' component={PostDetails}/>
         </Stack.Navigator>
     );
 }
