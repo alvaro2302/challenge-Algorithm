760e58dee4bd895a275f4a9b76acc1dde727dff0
alvaro2302 <alvaro230297@hotmail.com>
2022-06-01 05:18:25 -0400

    add FlatList for view post List from id searched

--- src/components/Posts/PostView.js
+++ src/components/Posts/PostView.js
@@ -1,13 +1,42 @@
-import React from "react";
-import {View,Text} from 'react-native';
+import React, {useEffect,useState}from "react";
+import {StyleSheet,FlatList,View} from 'react-native';
+import PostServices from "../../services/PostServices";
 
+import Post from "./Post";
 const PostView = (props)=>{
-    const {data} = props; 
+    const {data} = props;
+    const [posts,setPost] = useState(null);
+    const getPost = async()=>{
+        const resultsPosts = await PostServices.getPosts(data.id);
+        console.log("posts,",resultsPosts);
+        setPost(resultsPosts);
+    }
+    const renderPost = ({item}) =>{
+        return (
+        <Post
+          idPhoto={item.id}
+          title={item.title} 
+        />
+        )
+    }
+
+    useEffect(()=>{
+        getPost();
+    },[])
     return (
-        <View style={{marginTop:'20%'}}>
-            <Text>List Posts</Text>
-            <Text>{data.id}</Text>
-        </View>
+         
+            <FlatList
+             data={posts}
+             renderItem={renderPost}
+             keyExtractor={(item)=>item.id}
+             showsVerticalScrollIndicator={false}
+            />       
+            
     )
 }
+const styles = StyleSheet.create({
+    container:{
+
+    }
+})
 export default PostView;
